d1522de030e1e53561608e55938e17c16b34ae4f
matshp0 <matvii.shpyruk@gmail.com>
2025-01-03 23:09:18 +0200

    sql fix

--- setup.sql
+++ setup.sql
@@ -3,26 +3,26 @@ CREATE SCHEMA IF NOT EXISTS "apiDB";
 
 CREATE TYPE user_role AS ENUM ('admin', 'user');
 
-CREATE TABLE IF NOT EXISTS "Account" (
+CREATE TABLE IF NOT EXISTS "apiDB"."Account" (
   "id" INTEGER PRIMARY KEY,
   "createdAt" TIMESTAMP DEFAULT NOW(),
   "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS "Question" (
+CREATE TABLE IF NOT EXISTS "apiDB"."Question" (
      "id" INTEGER PRIMARY KEY,
      "createdAt" TIMESTAMP DEFAULT NOW(),
      "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS "Office" (
+CREATE TABLE IF NOT EXISTS "apiDB"."Office" (
     "id" INTEGER PRIMARY KEY,
     "status" SMALLINT,
     "createdAt" TIMESTAMP DEFAULT NOW(),
     "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS "AccountOffice" (
+CREATE TABLE IF NOT EXISTS "apiDB"."AccountOffice" (
    "accountId" INTEGER NOT NULL REFERENCES "Account" (id),
    "officeId" INTEGER NOT NULL REFERENCES "Office" (id),
    "createdAt" TIMESTAMP DEFAULT NOW(),
@@ -30,13 +30,13 @@ CREATE TABLE IF NOT EXISTS "AccountOffice" (
    PRIMARY KEY ("accountId", "officeId")
 );
 
-CREATE TABLE "Date" (
+CREATE TABLE IF NOT EXISTS "apiDB"."Date" (
     "dateValue" DATE PRIMARY KEY,
     "createdAt" TIMESTAMP DEFAULT NOW(),
     "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS "DateOffice" (
+CREATE TABLE IF NOT EXISTS "apiDB"."DateOffice" (
     "officeId" INTEGER NOT NULL REFERENCES "Office" (id),
     "date" DATE NOT NULL,
     "questionId" INTEGER NOT NULL REFERENCES "Question" (id),
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS "DateOffice" (
    PRIMARY KEY ("officeId", "date", "questionId")
 );
 
-CREATE TABLE IF NOT EXISTS "AccountQuestion" (
+CREATE TABLE IF NOT EXISTS "apiDB"."AccountQuestion" (
     "questionId" INTEGER NOT NULL REFERENCES "Question" (id),
     "accountId" INTEGER NOT NULL REFERENCES "Account" (id),
     "createdAt" TIMESTAMP DEFAULT NOW(),
@@ -55,35 +55,27 @@ CREATE TABLE IF NOT EXISTS "AccountQuestion" (
 );
 
 
-CREATE TABLE IF NOT EXISTS "AccountOffice" (
-   "accountId" INTEGER NOT NULL REFERENCES "Account" (id),
-   "officeId" INTEGER NOT NULL REFERENCES "Office" (id),
-   "createdAt" TIMESTAMP DEFAULT NOW(),
-   "updatedAt" TIMESTAMP DEFAULT NOW(),
-   PRIMARY KEY ("accountId", "officeId")
-);
-
 
 DO $$
     BEGIN
         FOR i IN 151..200 LOOP
-                INSERT INTO "Office" ("id", "status", "createdAt", "updatedAt")
+                INSERT INTO "apiDB"."Office" ("id", "status", "createdAt", "updatedAt")
                 VALUES (i, 4, NOW(), NOW());
             END LOOP;
     END $$;
 
-INSERT INTO "Question" ("id")
+INSERT INTO "apiDB"."Question" ("id")
 VALUES ('56');
-INSERT INTO "Question" ("id")
+INSERT INTO "apiDB"."Question" ("id")
 VALUES ('55');
 
-INSERT INTO "Account" ("id")
+INSERT INTO "apiDB"."Account" ("id")
 VALUES ('725433593');
 
-INSERT INTO "AccountQuestion" ("questionId", "accountId")
+INSERT INTO "apiDB"."AccountQuestion" ("questionId", "accountId")
 VALUES ('56', '725433593');
 
-INSERT INTO "AccountQuestion" ("questionId", "accountId")
+INSERT INTO "apiDB"."AccountQuestion" ("questionId", "accountId")
 VALUES ('55', '725433593');
 
 
