264356aca82c227ba2774abe4f92db8ded32c5e0
matshp0 <matvii.shpyruk@gmail.com>
2025-01-08 23:37:26 +0200

    fix

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const headers = {
   'sec-fetch-site': 'same-origin',
   'x-csrf-token': 'eSEdUo-0z58vXYpwVQM-fpwiNad2O372wp48Uzn9auk1aGg-4P-GqRkO8zsmQHhP3xBx3ixVGJ2v020nf4UihA==',
   'x-requested-with': 'XMLHttpRequest',
-  'cookie': 'WEBCHSID2=uut902rs7nrl9n2cpf55o8scgt; _identity=d7ba306c964acac74531a39449c1cdd26428e3a54678c1c8cd1bf7f9cc702f15a%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A20%3A%22%5B2213638%2Cnull%2C28800%5D%22%3B%7D; _csrf=656d4675892983b9f3a3fbdcbab93621b823b583bb951285aa36ee0b526dadafa%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22LIuloKI66SyKsCF1C2DyZnfkmMQtFxHm%22%3B%7D',
+  'cookie': 'WEBCHSID2=uut902rs7nrl9n2cpf55o8scg; _identity=d7ba306c964acac74531a39449c1cdd26428e3a54678c1c8cd1bf7f9cc702f15a%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A20%3A%22%5B2213638%2Cnull%2C28800%5D%22%3B%7D; _csrf=656d4675892983b9f3a3fbdcbab93621b823b583bb951285aa36ee0b526dadafa%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22LIuloKI66SyKsCF1C2DyZnfkmMQtFxHm%22%3B%7D',
   'Referer': 'https://eq.hsc.gov.ua/site/step2?chdate=2025-01-28&question_id=55&id_es=',
   'Referrer-Policy': 'strict-origin-when-cross-origin'
 };
